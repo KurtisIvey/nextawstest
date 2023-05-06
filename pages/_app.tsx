07aef8800e5ebec3b17ca5651e9a62e7740a23bd
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-06 00:46:26 -0500

    update import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import { NextUIProvider, createTheme } from "@nextui-org/react";
 import { Amplify } from "aws-amplify";
 import awsExports from "../src/aws-exports";
-
 Amplify.configure({ ...awsExports, ssr: true });
 
 const darkTheme = createTheme({ type: "dark" });
