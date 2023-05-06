454149a5450d60aa8555406c58a866435113932c
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-06 14:15:20 -0500

    remove aws amplify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextUIProvider, createTheme } from "@nextui-org/react";
-import { Amplify } from "aws-amplify";
-import awsExports from "../src/aws-exports";
-Amplify.configure({ ...awsExports, ssr: true });
 
 const darkTheme = createTheme({ type: "dark" });
 
